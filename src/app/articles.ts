a7a8e8bbec85dfdb77898dfefeeeda3a480a9e07
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2024-03-20 20:10:34 +0200

    Project2

---
+++ src/app/articles.ts
@@ -0,0 +1,36 @@
+export interface Article {
+    src: string;
+    title: string;
+    text: string;
+  }
+  
+  export const articles: Article[] = [
+    {
+      src: 'assets/images/1.jpg',
+      title: 'Free Entertainment',
+      text: 'Our advanced, touch-screen digital entertainment system puts you in charge of your entertainment options.'
+    },
+    {
+      src: 'assets/images/2.jpg',
+      title: 'Turn Dreaming',
+      text: 'Customize your ideal vacation to 300+ destinations worldwide with Delta Vacations.'
+    },
+    {
+      src: 'assets/images/3.jpg',
+      title: 'Gift Card',
+      text: 'Give the Gift of Go™ and let them explore more than 300 destinations worldwide. Terms apply. Shop now.'
+    }
+  ];
+  export const deals: Article[] = [
+    {
+      src: 'assets/images/4.jpg',
+      title: 'Deals & Destinations',
+      text: 'Our advanced, touch-screen digital entertainment system puts you in charge of your entertainment options.'
+    },
+    {
+      src: 'assets/images/4.jpg',
+      title: 'Turn Dreaming',
+      text: 'Customize your ideal vacation to 300+ destinations worldwide with Delta Vacations.'
+    },
+  ];
+  
